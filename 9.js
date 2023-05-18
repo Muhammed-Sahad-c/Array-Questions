85438f22c90b2cf84a02b7e934422d0e5c73a3c4
Emmanuel James <93952415+Deerah1234@users.noreply.github.com>
2023-05-18 23:03:40 +0100

    Update 9.js

--- 9.js
+++ 9.js
@@ -6,7 +6,7 @@ var array2 = [6, 7, 8, 9, 10, 11];
 var length1 = array1.length;
 var length2 = array2.length
 
-for (i = 0; i < length2; i++) {
+for (let i = 0; i < length2; i++) {
     array1[length1] = array2[i];
     length1++;
 }
