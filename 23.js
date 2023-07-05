4551d4b88882c4032643e27961fe417269c8a770
Muhammed Sahad C <109616941+00sahad@users.noreply.github.com>
2023-07-05 10:02:10 +0530

    23.js

--- 23.js
+++ 23.js
@@ -23,3 +23,16 @@ for(i=0;i<length;i++){
 
 
 console.log(resultArray)
+
+
+// method 2 using inbuilt methods
+
+var array = [1,2,3,4,0,0,2,0];
+
+for(let i=0;i<array.length;i++){
+    if(array[i]==0){
+         array.unshift(array.splice(i,1)[0])
+    }
+}
+
+console.log(array)
